e400a8eb0f915d3e16cf1f797cc4cf97c6e77bf6
Aagya-Sharma <sharmaaagya7@gmail.com>
2023-10-13 17:05:22 +0545

    include navbar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import {  NavLink, useNavigate } from "react-router-dom";
+import "../assets/css/NavBar.css";
+import { useForumStore } from '../store';
+
+function Navbar() {
+  const navigate = useNavigate();
+  const { logout, setUser } = useForumStore();
+
+  const handleLogout = () => {
+    logout();
+    //set the user state to {}
+    setUser({});
+    //navigate to login screen
+    navigate("/");
+  };
+
+  return (
+    <div className="navbar">
+      <NavLink to="/forums" activeClassName="active" className="forums-button">
+        Forum
+      </NavLink>
+      <NavLink to="/forum-creation" activeClassName="active" className="forums-button">
+        Add Forum
+      </NavLink>
+      <button onClick={handleLogout} className="logout-button">
+        Logout
+      </button>
+    </div>
+  );
+}
+
+export default Navbar;
